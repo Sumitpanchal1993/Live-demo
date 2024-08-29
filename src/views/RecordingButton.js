5ef30dbf60fbb9fabe48fe9b53ebb2595bf10167
Manoj Gavel <manoj.gav5393@vagaro.com>
2024-08-29 13:34:20 +0530

    recording button addded server call id is ongoing

---
+++ src/views/RecordingButton.js
@@ -0,0 +1,43 @@
+import { CustomCallControlButtonCallback, CustomCallControlButtonCallbackArgs } from "@azure/communication-react";
+import { Record20Regular, RecordStop20Filled } from "@fluentui/react-icons";
+// import { startRecording, stopRecording } from "./Api";
+import { registerIcons } from "@fluentui/react";
+
+registerIcons({
+    icons: {
+        StartRecording: <Record20Regular />,
+        StopRecording: <RecordStop20Filled />
+    }
+});
+
+export const recordingButtonPropsCallback = (serverCallId, recordingId, setRecordingId) => {
+    const isRecording = !!recordingId;
+    return (args) => ({
+        placement: 'primary',
+        key: 'recordingButton',
+        showLabel: true,
+        text: isRecording ? 'Stop Recording' : 'Start Recording',
+        iconName: isRecording ? 'StopRecording' : 'StartRecording',
+        onItemClick: async () => {
+            if (!serverCallId) {
+                console.warn('Recording buton: No serverCallId yet!');
+                return;
+            }
+
+            if (isRecording) {
+                // stop the recording
+                // await stopRecording({ serverCallId, recordingId });
+                console.log('STOP RECORDNG')
+                setRecordingId('');
+                return
+            }
+
+            // start the recording
+            // const { recordingId: newRecordingId } = await startRecording({ serverCallId });
+            const { recordingId: newRecordingId } =  {
+                recordingId:'12455RECORDINGID'
+            };
+            setRecordingId(newRecordingId);
+        }
+    });
+}
